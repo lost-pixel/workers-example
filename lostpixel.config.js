8cd281392b39f617c9774cc07a2c7743e4dda7f5
d-ivashchuk <dimaiv@me.com>
2023-05-13 19:22:13 +0200

    new run

---
+++ lostpixel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  customShots: {
+    currentShotsPath: "./lost-pixel",
+  },
+  lostPixelProjectId: "clhm95beq00pij00e1dflanm5",
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+  lostPixelPlatform: "https://api.staging.lost-pixel.com",
+};
